0a614b498f320689a14381f3ed102d2ceabb675b
Gangbean <xbox2537@gmail.com>
2024-01-01 17:03:08 +0900

    feat: Multiply backward 테스트 구현 #15

--- src/test/multiply_test.py
+++ src/test/multiply_test.py
@@ -11,3 +11,14 @@ class MultiplyTest(unittest.TestCase):
         y = multiply(a, b)
 
         self.assertAlmostEqual(y.data, 2.0)
+    
+    def test_Multiply_backward를_통해_최종결과에대한_입력값별_기울기를_구할수있습니다(self):
+        a = Variable(np.array(1.0))
+        b = Variable(np.array(2.0))
+
+        y = multiply(a, b)
+
+        y.backward()
+
+        self.assertEqual(a.grad, 2.0)
+        self.assertEqual(b.grad, 1.0)
